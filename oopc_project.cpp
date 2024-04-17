cdaf52d24f065807a369b4ba79e3fb9fc4377c1c
aumbshah <148608460+aumbshah@users.noreply.github.com>
2024-04-17 09:46:06 +0530

    Update oopc_project.cpp

--- oopc_project.cpp
+++ oopc_project.cpp
@@ -1,1 +1,27 @@
 \\This project is prepared by 23CS085, 23CS089 & 23CS106
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+class RadixConverter {
+private:
+    string number;
+    int frombase;
+    int tobase;
+
+public:
+    RadixConverter(string num, int from, int to): number(num) , frombase(from), tobase(to)
+     {}
+    int todecimal()
+    {
+        int decimal = 0;
+        int power = 0;
+        for (int i = number.size() - 1; i >= 0; i--) {
+            int digit_value;
+            if (isdigit(number[i])) {
+                digit_value = number[i] - '0';
+            } else {
+                digit_value = number[i] - 'A' + 10;
+            }  
